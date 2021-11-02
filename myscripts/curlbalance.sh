51a94a8eab98935b0a123c1b0a63e636f3327235
designcrewz.alan <itzjustalan@gmail.com>
2021-11-02 23:40:30 +0530

    test

--- myscripts/curlbalance.sh
+++ myscripts/curlbalance.sh
@@ -2,6 +2,7 @@
 
 #ASIANET_SUB_CODE='KT66353'
 
+echo "curl 'https://myabb.in/totalBalance' --data-raw subscriberCode=$ASIANET_SUB_CODE"
 LC_resp=$(curl 'https://myabb.in/totalBalance' --data-raw "subscriberCode=$ASIANET_SUB_CODE")
 
 LC_resp=$(echo "$LC_resp" | tr ',' '\n')
@@ -11,11 +12,9 @@ LC_total_total=$(echo "$LC_usage" | cut -d'.' -f1 | sed -n '2 p')
 LC_total_left=$(echo "$LC_usage" | cut -d'.' -f1 | sed -n '1 p')
 LC_total_used=$(echo "$LC_usage" | cut -d'.' -f1 | sed -n '3 p')
 
-LC_pretty_out="
-$ASIANET_SUB_CODE $LC_connType CONN
+LC_pretty_out="$ASIANET_SUB_CODE $LC_connType CONN
 TOTAL: $(echo "scale=2;$LC_total_total/1024" | bc -l) GiB
 LEFT: $(echo "scale=2;$LC_total_left/1024" | bc -l) GiB
-USED: $(echo "scale=2;$LC_total_used/1024" | bc -l) GiB
-"
+USED: $(echo "scale=2;$LC_total_used/1024" | bc -l) GiB"
 #echo "$LC_pretty_out"
 notify-send "$LC_pretty_out"
--- myscripts/spinup.sh
+++ myscripts/spinup.sh
@@ -4,19 +4,28 @@ declare -A apps=(
 ["aap"]="app-admin-api"
 ["aau"]="app-admin-ui"
 ["asp"]="app-salesman-api"
-["asx"]="app-sales-executive-api"
+["axp"]="app-sales-executive-api"
 ["acp"]="app-customer-api"
 )
 declare -A ports=(
-["aap"]="3000:8000"
-["aau"]="80:80"
-["asp"]="3003:3003"
-["asx"]="3001:5000"
-["acp"]="3002:3002"
+["aap"]="8001:8000"
+["aau"]="8000:80"
+["asp"]="8004:3003"
+["axp"]="8003:5000"
+["acp"]="8002:3002"
 )
 
-testContainers() {
-	echo "test_"
+# options
+
+DRYRUN=false				# build image from scratch
+
+
+
+# curl them down
+testcontainers() {
+	echo "test:"
+	sudo docker images
+	echo ""
 	sudo docker ps
 	echo ""
 	for app in "${!apps[@]}"; do
@@ -31,13 +40,34 @@ testContainers() {
 	echo ""
 }
 
-spinup() {
+
+# startcontainer or build if no image
+startcontainer() {
+	app="$1";
+	if $DRYRUN ; then
+		buildcontainer "$app"
+	else
+		# check if image exists
+		image=$(sudo docker images | grep "${apps[$app]}")
+		if [ -z "$image" ]; then
+			buildcontainer "$app"
+		else
+			echo ""
+			echo "image found: $image"
+			echo ""
+			sudo docker run -p "${ports[$app]}" -d "${apps[$app]}"
+		fi
+	fi
+}
+
+# update code and generate build
+buildcontainer() {
 	app=$1;
 	echo "updating ${apps[$app]}"
 	cd "$HOME/apps/${apps[$app]}/" || return
 
 	# fetch changes on testing beranch
-	git pull origin testing
+	git pull origin staging
 
 	# stop docker container
 	containerId=$(sudo docker ps | grep "${apps[$app]}" | cut -d' ' -f1)
@@ -46,19 +76,18 @@ spinup() {
 	fi
 
 	# stop customer api to free up space
-	containerId=$(sudo docker ps | grep app-customer-api | cut -d' ' -f1)
-	if [ -n "$containerId" ]; then
-		sudo docker stop "$containerId"
-	fi
+	#containerId=$(sudo docker ps | grep app-customer-api | cut -d' ' -f1)
+	#if [ -n "$containerId" ]; then
+	#	sudo docker stop "$containerId"
+	#fi
+
+	# copy custom staging files for aau
 
 	# clean unused containers to free up space
 	sudo docker system prune -af
 
-	sudo docker images
-	sudo docker ps
-
 	# build docker container
-	sudo docker build -t "${apps[$app]}" "$HOME/apps/${apps[$app]}/ -q"
+	sudo docker build -t "${apps[$app]}" "$HOME/apps/${apps[$app]}/"
 
 	# run docker container
 	sudo docker run -p "${ports[$app]}" -d "${apps[$app]}"
@@ -72,46 +101,98 @@ spinAll() {
 	done
 }
 
-showHelpAndExit() {
+showhelp() {
 	echo ""
 	echo "	app not specified!"
 	echo ""
-	echo "	registered apps: ${!apps[*]} --all test"
+	echo "	registered apps: ${!apps[*]} --all --test"
 	echo ""
-	exit 1;
 }
 
-if [ -z "$1" ]; then
-	showHelpAndExit
-elif [ $# -gt 1 ]; then
-	echo ""
-	echo "	more than one argument specified!"
-	echo ""
-	exit 1;
-else
-	if [ "$1" = "test" ]; then
-		testContainers
-	elif [ "$1" = "--all" ]; then
-		# stop all containers
-		sudo docker kill "$(sudo docker ps -q)"
+#if [ -z "$1" ]; then
+#	showhelp
+#	exit 1;
+#elif [ $# -gt 1 ]; then
+#	echo ""
+#	echo "	more than one argument specified!"
+#	echo ""
+#	exit 1;
+#else
+#	if [ "$1" = "test" ]; then
+#		testcontainers
+#	elif [ "$1" = "--all" ]; then
+#		# stop all containers
+#		sudo docker kill "$(sudo docker ps -q)"
+#
+#		# start all containers
+#		for app in "${!apps[@]}"; do
+#			echo "=================================================================="
+#			echo "	${apps[$app]}"
+#			echo "=================================================================="
+#			buildcontainer "$app"
+#		done
+#	elif [ -z "${apps[$1]}" ]; then
+#		showhelp
+#	else
+#		echo "buildcontainer"
+#		buildcontainer "$1"
+#		testcontainers
+#	fi
+#fi
 
-		# spinup all containers
-		for app in "${!apps[@]}"; do
-			echo "=================================================================="
-			echo "	${apps[$app]}"
-			echo "=================================================================="
-			spinup "$app"
-		done
-	elif [ -z "${apps[$1]}" ]; then
-		showHelpAndExit
-	else
-		echo "spinup"
-		spinup "$1"
-		testContainers
-	fi
+
+#testcontainers
+#buildcontainer aaa
+#cd ~/wp
+
+usage() { echo "Usage: $0 [-s <45|90>] [-p <string>]" 1>&2; exit 1; }
+
+if [ -z "$1" ]; then
+	showhelp
+else
+	# initialise options
+	for arg in "$@"; do
+		case "$arg" in
+			-d|--dry)
+				DRYRUN=true
+				;;
+		esac
+	done
+
+	# handle arguments
+	for arg in "$@"; do
+		case "$arg" in
+			-t|--test)
+				#testcontainers
+				;;
+			-a|--all)
+				if "$DRYRUN" ; then
+					# stop all containers
+					sudo docker kill "$(sudo docker ps -q)"
+				fi
+
+				# start all containers
+				for app in "${!apps[@]}"; do
+					echo "=================================================================="
+					echo "	${apps[$app]}"
+					echo "=================================================================="
+					startcontainer "$app"
+				done
+				;;
+			-d|--dry)
+				# skip flag
+				;;
+			*)
+				if [ -z "${apps[$arg]}" ]; then
+					echo "$arg not found"
+					showhelp
+				else
+					startcontainer "$arg"
+				fi
+		esac
+	done
+
+	# test all containers
+	testcontainers
 fi
 
-
-#testContainers
-#spinup aaa
-#cd ~/wp
