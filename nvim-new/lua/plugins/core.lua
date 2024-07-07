92057dde4187d97b67e7d2ff943ea2dd00ba603c
Alan K John <itzjustalan@gmail.com>
2024-07-08 00:00:57 +0530

    svelte and go

--- nvim-new/lua/plugins/core.lua
+++ nvim-new/lua/plugins/core.lua
@@ -6,12 +6,15 @@ return {
     },
   },
   { import = "lazyvim.plugins.extras.editor.harpoon2" },
+  { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.svelte" },
   -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
   { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
   { import = "lazyvim.plugins.extras.coding.copilot" },
+  { import = "lazyvim.plugins.extras.coding.luasnip" },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
