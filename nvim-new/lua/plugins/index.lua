348afd555b6e0d0f78dfa7db3ce037f2988c0d85
Alan K John <itzjustalan@gmail.com>
2024-08-26 15:19:29 +0530

    reset

--- nvim-new/lua/plugins/index.lua
+++ nvim-new/lua/plugins/index.lua
@@ -11,6 +11,7 @@ return {
   { import = "lazyvim.plugins.extras.formatting.prettier" },
   { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.svelte" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
